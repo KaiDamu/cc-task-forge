bfdc4ec1caf96eea4afeb8b32827f2d17b010083
KaiDamu <kaidamumail@gmail.com>
2026-04-15 12:21:04 +0200

    Fix logging format in os.pullEventRaw hook to ensure proper event serialization

--- install/script.lua
+++ install/script.lua
@@ -3,7 +3,7 @@ local osPullEventRawOriginal = os.pullEventRaw
 os.pullEventRaw = function(...)
     local evt = { osPullEventRawOriginal(...) }
     if tf and tf.logWrite then
-        tf.logWrite("[HOOK] " .. evt[1] .. " - " .. textutils.serialize(evt))
+        tf.logWrite("[HOOK] " .. (tostring(evt[1]) or "") .. " - " .. (tostring(evt[2]) or ""))
     end
     return table.unpack(evt)
 end
